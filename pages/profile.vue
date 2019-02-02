b381112b622504e36b0754ed78e03333b41c023f
Puriya <puriya.fd@gmail.com>
2019-02-02 18:16:27 +0330

    Remove TheLogin & TheProfile and use just pages for it

---
+++ pages/profile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header />
+    <div class="container">
+      <div class="row">
+        <div class="card col-md-8">
+          <h3 class="card-header">
+            Your profile
+          </h3>
+          <div class="card-block">
+            <h4 class="card-title">
+              Special title treatment
+            </h4>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheProfile'
+}
+</script>
+
+<style scoped>
+</style>
