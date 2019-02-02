df18c26a9891e018d15461dd7eb3a5b73c8b2b0d
Puriya <puriya.fd@gmail.com>
2019-02-02 12:41:00 +0330

    Redirect from login page if user has already logged in

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -3,7 +3,11 @@ export default function({ store, route, redirect }) {
   // if (url.indexOf('/#access_token') !== -1) {
   // const accessToken = url.match(/#(?:access_token)=([\S\s]*?)&/)[1]
   // }
+  if (route.path === '/login' && store.getters.isLoggedIn) {
+    return redirect('/')
+  }
   if (route.path === '/login' || url.indexOf('/#access_token') !== -1) return
+
   if (!store.getters.isLoggedIn) {
     return redirect('/login')
   }
