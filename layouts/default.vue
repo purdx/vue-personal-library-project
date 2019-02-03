c7384fdf7ec1d59282eae8d42aabee999d8fdf6f
Puriya <puriya.fd@gmail.com>
2019-02-03 16:14:55 +0330

    Add Notification component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <nuxt />
+    <Notification />
   </div>
 </template>
+
+<script>
+import Notification from '@/components/Notification'
+export default {
+  components: {
+    Notification
+  }
+}
+</script>
 
 <style>
 </style>
