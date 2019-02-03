ac0eb4fc86df50e5ac2c097925bc69a778104531
Puriya <puriya.fd@gmail.com>
2019-02-03 10:51:26 +0330

    Add update info functionality to profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -18,7 +18,7 @@
                 </li>
               </ul>
               <div class="profile-buttons">
-                <button type="button" class="btn btn-outline-secondary">
+                <button type="button" class="btn btn-outline-secondary" @click="showInfoModal">
                   Edit Info
                 </button>
                 <button type="button" class="btn btn-outline-secondary">
@@ -29,16 +29,87 @@
           </div>
         </div>
       </div>
+      <Modal v-if="infoModal" ref="infoModal">
+        <h3 slot="modal-title">
+          Update your info
+        </h3>
+        <form slot="body" @submit.prevent>
+          <div class="form-group">
+            <label for="nameInput">
+              Name
+            </label>
+            <input
+              id="nameInput" 
+              v-model="newName"
+              type="text"
+              class="form-control"
+              placeholder="Enter name"
+            >
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">
+              Email
+            </label>
+            <input
+              id="exampleInputEmail1"
+              v-model="newEmail"
+              type="email"
+              class="form-control"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            >
+          </div>
+          <button type="submit" class="btn btn-primary" @click="updateInfo">
+            Submit
+          </button>
+        </form>
+      </Modal>
     </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Modal from '@/components/Modal'
 export default {
   name: 'TheProfile',
   components: {
-    Header
+    Header,
+    Modal
+  },
+  data() {
+    return {
+      infoModal: false,
+      newName: '',
+      newEmail: ''
+    }
+  },
+  methods: {
+    showInfoModal() {
+      this.infoModal = true
+      setTimeout(() => {
+        this.$refs.infoModal.show()
+      }, 0)
+    },
+    updateInfo() {
+      this.$axios({
+        method: 'PATCH',
+        url: 'http://localhost:3000/api/user',
+        data: {
+          name: this.newName,
+          email: this.newEmail
+        }
+      })
+        .then(response => {
+          this.infoModal = false
+          this.$store.commit('SET_USERNAME', response.data.data.name)
+          this.$store.commite('SET_EMAIL', response.data.data.name)
+        })
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          console.log(error)
+        })
+    }
   }
 }
 </script>
