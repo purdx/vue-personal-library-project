e4a32dfa0126110fc92a1384e28e958d4dc79ecb
Puriya <puriya.fd@gmail.com>
2019-02-03 10:44:17 +0330

    Remove extra localStorage setItem from index page

--- pages/index.vue
+++ pages/index.vue
@@ -15,14 +15,11 @@ export default {
     const url = this.$route.fullPath
     if (url.indexOf('/#access_token') !== -1) {
       const accessToken = url.match(/#(?:access_token)=([\S\s]*?)&/)[1]
-      localStorage.setItem('accessToken', accessToken)
       this.$store.commit('SET_TOKEN', accessToken)
       this.$axios('http://localhost:3000/api/user')
         .then(response => {
           this.$store.commit('SET_USERNAME', response.data.data.name)
-          localStorage.setItem('userName', response.data.data.name)
           this.$store.commit('SET_EMAIL', response.data.data.email)
-          localStorage.setItem('email', response.data.data.email)
         })
         .catch(response => {
           alert(response)
