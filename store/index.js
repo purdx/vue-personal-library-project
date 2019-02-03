e22b7af10703dce1f6ea4ac5dac90b0ab7d48ffe
Puriya <puriya.fd@gmail.com>
2019-02-03 10:42:33 +0330

    Improve Vuex mutations presistence

--- store/index.js
+++ store/index.js
@@ -12,15 +12,18 @@ export const getters = {
 
 export const mutations = {
   SET_TOKEN(state, accessToken) {
+    localStorage.setItem('accessToken', accessToken)
     state.accessToken = accessToken
   },
   REMOVE_USER(state) {
     Object.keys(state).forEach(prop => (state[prop] = null))
   },
   SET_USERNAME(state, userName) {
+    localStorage.setItem('userName', userName)
     state.userName = userName
   },
   SET_EMAIL(state, email) {
+    localStorage.setItem('email', email)
     state.email = email
   }
 }
