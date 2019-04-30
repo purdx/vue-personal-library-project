b162c4d046342685ca227c94b1083ba97060c893
purdx <puriya.fd@gmail.com>
2019-04-30 15:18:31 +0430

    Remove unnecessary comment

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,8 +1,5 @@
 export default function({ store, route, redirect }) {
   const url = route.fullPath
-  // if (url.indexOf('/#access_token') !== -1) {
-  // const accessToken = url.match(/#(?:access_token)=([\S\s]*?)&/)[1]
-  // }
   if (route.path === '/login' && store.getters.isLoggedIn) {
     return redirect('/')
   }
